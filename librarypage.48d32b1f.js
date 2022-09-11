b58b40e52590ff4c69dc7b2df06b5e94a884b57f
DmitriyR90 <99822054+DmitriyR90@users.noreply.github.com>
2022-09-11 13:01:01 +0000

    Deploying to gh-pages from @ DmitriyR90/filmoteka-team3@32f386461d4d4313ecf0e79a856602bdcfa82e29 🚀

--- librarypage.48d32b1f.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[modal-developers-open]"),closeModalBtn:document.querySelector("[modal-developers-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden__developers")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o),e.modal.addEventListener("click",(function(e){e.currentTarget===e.target&&o()}))})();document.querySelector(".info").classList.remove("is-stealth");
-//# sourceMappingURL=librarypage.48d32b1f.js.map
