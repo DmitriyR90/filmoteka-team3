3d97c537bd99916ea8d99f81e80df85782d11a17
DmitriyR90 <99822054+DmitriyR90@users.noreply.github.com>
2022-09-11 08:39:54 +0000

    Deploying to gh-pages from @ DmitriyR90/filmoteka-team3@7ac20332727cd91103fcb290043610cb67e0b30e 🚀

--- librarypage.54498feb.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[modal-developers-open]"),closeModalBtn:document.querySelector("[modal-developers-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden__developers")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o),e.modal.addEventListener("click",(function(e){e.currentTarget===e.target&&o()}))}(),document.querySelector(".info").classList.remove("is-hidden");
-//# sourceMappingURL=librarypage.54498feb.js.map
